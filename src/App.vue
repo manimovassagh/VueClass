51188fb91af53f0a99e7815112a16a9aafcc4ac4
manimovassagh <mani.mgh@gmail.com>
2021-11-11 21:32:04 +0100

    added´

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,73 @@
 <template>
-  <v-app>
-    <v-container>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/pedex">pedex</router-link>
-      <router-link to="/cc">CC</router-link>
-    </v-container>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Title</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab :to="links.home">Home</v-tab>
+          <v-tab :to="links.about">about</v-tab>
+          <v-tab :to="links.contact">Contact</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-3"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+
+  </v-card>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-
-export default Vue.extend({
-  name: "App",
-  components: {},
-  data: () => ({
-    //
-  }),
-});
+<script>
+  import Home from './views/Home.vue'
+export default {
+  components:{
+Home
+  },
+  data(){
+    return{
+      links:{
+        home:'/',
+        about:'/about',
+        contact:'/contact'
+      }
+    }
+  }
+}
 </script>
+
